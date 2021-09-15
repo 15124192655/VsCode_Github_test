b103f29ec15678aaf769023c07cf8696dd32d70d
骆禹松 <916037541@qq.com>
2021-09-15 09:24:35 +0800

    提交

---
+++ test.cpp
@@ -0,0 +1,23 @@
+// 实现简单加法计算器
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"请输入一个数字"<<endl;
+    cin>>a;
+    cout<<"请再次输入一个数字"<<endl;
+    cin>>b;
+    cout<<a+b<<endl;
+    // system("pause");
+}
+
+// // 方法2
+// int add(int a, int b){
+//     return a+b;
+// }
+// int main(){
+//     int result = add(5,10);
+//     cout<<"结果为"<<result<<endl;
+//     return 0;
+// }
